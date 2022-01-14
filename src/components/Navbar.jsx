108bdae846648c292cc5d7dc057c5eadf87b786e
Alison <merendaakina@gmail.com>
2022-01-14 09:05:26 -0800

    Adicionando styled-components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height:60px;
+`
+const Wrapper = styled.div`
+    padding: 10px;
+`
+
+const Navbar = () => {
+    return (
+        <Container>
+            Página Inicial
+        </Container>
+    )
+}
+
+export default Navbar
