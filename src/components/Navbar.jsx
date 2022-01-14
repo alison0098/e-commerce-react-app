128cf2da7ffe645bb890f2a749b0884b1305146f
Alison <merendaakina@gmail.com>
2022-01-14 11:10:02 -0800

    Adicionando barra de pesquisa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,54 @@
+import { Search } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
     height:60px;
-`
+`;
 const Wrapper = styled.div`
-    padding: 10px;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-itens: center;
+`;
+
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
 `
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`;
+const Center = styled.div`
+flex: 1;
+`;
+const Right = styled.div`
+flex: 1;
+`;
 
 const Navbar = () => {
     return (
         <Container>
-            Página Inicial
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        input
+                        <Search></Search>
+                    </SearchContainer>                    
+                </Left>
+                <Center>
+                    Centro
+                </Center>
+                <Right>
+                    Direita
+                </Right>
+            </Wrapper>
         </Container>
     )
 }
