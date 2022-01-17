7ec949d66482099924814970364fb8d7d58612f6
Alison <merendaakina@gmail.com>
2022-01-17 12:22:07 -0800

    Adicionando slider

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 const Container = styled.div`
     height: 30px;
-    background-color: #416a59;
+    background-color: #009B77;
     color: white;
     display : flex;
     align-items : center;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,15 +1,40 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons"
 import styled from "styled-components"
 
 const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    background-color: #55B4B0;
+    position: relative;
+
+`
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #f3eded; 
+    border-radius: 50%;
+    display : flex;
+    align-items : center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
 `
 
 const Slider = () => {
     return (
         <Container>
-            <Arrow></Arrow>
+            <Arrow direction="left">
+                <ArrowLeftOutlined/>
+            </Arrow>
+            <Arrow direction="right">
+                <ArrowRightOutlined/>
+            </Arrow>            
         </Container>
     )
 }
