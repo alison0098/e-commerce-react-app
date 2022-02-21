8c7134be31b28c0c7909e07093d307785aa8979a
Alison <merendaakina@gmail.com>
2022-02-21 12:07:47 -0800

    Adicionando informacoes nas categorias

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,12 +2,52 @@ import styled from 'styled-components'
 import React from 'react'
 
 
-const Container = styled.div``
+
+const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+    height: 70vh;
+    position: relative;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+const Title = styled.h1`
+    color: #009B77;
+    margin-bottom: 20px;
+
+`
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background-color: white;
+    color:gray;
+    cursor: pointer;
+`
 
 const CategoryItem = ({item}) => {
     return (
         <Container>
-            Testando
+            <Image src={item.img}/>
+            <Info>
+                <Title>
+                    {item.title}
+                </Title>
+                <Button> COMPRAR </Button>
+            </Info>
         </Container>
     )
 }
--- src/data.js
+++ src/data.js
@@ -25,22 +25,22 @@ export const sliderItems = [
 export const categories = [
     {
         id:0,
-        img: "https://images.lojanike.com.br/produto/tenis-nike-free-metcon-4-feminino-CZ0596-669-1-11632235589.jpg",
-        title: "Tênis Nike Femininos",
+        img: "https://images.lojanike.com.br//produto/tenis-nike-court-royale-2-DH3159-001-1-11628872302.jpg",
+        title: "Tênis Casuais",
         desc: "O 4 traz de volta a tela super-resistente e ventilada da versão original. A parte de cima conta com uma alça inspirada no Huarache e um painel lateral com listras emborrachadas que envolve o seu calcanhar em movimentos multidirecionais.",
         bg:"d6fff1"
     },
     {
         id:1,
-        img: "https://images.lojanike.com.br/1024x1024/produto/blusao-m-nsw-spe-bb-aop-po-hoodie-rf-DM6881-010-1-11638989675.jpg",
-        title: "Jaquetas Nike Masculinas",
+        img: "https://images.lojanike.com.br/produto/camiseta-nike-dri-fit-mixed-relays-masculina-CW0937-673-1-11625230126.jpg",
+        title: "Blusas Esportivas",
         desc: "O Club Fleece não é um sucesso de vendas por acaso: ele apresenta uma superfície escovada para proporcionar isolamento térmico e maciez. O toque acolhedor e familiar torna esse tecido essencial para proteger seu corpo das temperaturas baixas.",
         bg:"defff4"
     },
     {
         id:2,
-        img: "https://images.lojanike.com.br/860x860/produto/camiseta-manga-curta-m-nk-oc-f-jdi-ss-te-DN3037-010-1-11638990712.jpg",
-        title: "Blusas Nike Masculinas",
+        img: "https://images.lojanike.com.br/produto/blusao-lal-m-nk-po-flc-essntl-team-DH9343-504-1-11638986626.jpg",
+        title: "Blusões",
         desc: "Diga as palavras e comece a agir com a camiseta Just Do It da Nike Basquete. Ela é feita de algodão macio e confortável em um caimento solto e descontraído.",
         bg:"e8fcf6"
     }
