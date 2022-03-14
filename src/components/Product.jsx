bd289863e9db47fbbbf365ce10aea878fba5e01e
Alison <merendaakina@gmail.com>
2022-03-14 17:19:40 -0300

    add: product effects

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,6 +5,22 @@ import {
 } from "@material-ui/icons";
 import styled from "styled-components";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(214,255,241,0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5 ease;
+  cursor: pointer;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -13,14 +29,44 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: d6fff1;
+  position: relative ;
+
+  &:hover ${Info}{
+    opacity: 1;
+  }
 `;
 
-const Circle = styled.div``;
+const Circle = styled.div`
+  width: 200px;
+  height:200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+
+
+`;
 const Image = styled.img`
 	height:75%;
+  z-index: 2;
 `;
-const Info = styled.div``;
-const Icon = styled.div``;
+
+const Icon = styled.div`
+  cursor : pointer;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 11px;
+  transition: all 0.4s ease;
+
+  &:hover{
+    transform: scale(1.4);
+  }
+`;
 
 const Product = ({item}) => {
   return (
